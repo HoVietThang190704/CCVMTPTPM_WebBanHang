602b7a8e24bc952ceaae2c1fecccc38d30b6d8fd
Trần Nguyễn Thiên Phước <tntphuoc1608@gmail.com>
2026-03-06 23:27:39 +0700

    Revert "feat: add product-detail page"

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -25,15 +25,3 @@ exports.addProduct = async (req, res) => {
     res.render('products/add', { error: err.message });
   }
 };
-
-exports.detail = async (req, res) => {
-  try {
-    const product = await productModel.getById(req.params.id);
-    if (!product) {
-      return res.status(404).send('Sản phẩm không tồn tại.');
-    }
-    res.render('products/detail', { product });
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-};
