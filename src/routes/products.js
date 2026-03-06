1909ff74a22b5b4a088a1072d17f6fc3ad61898f
Trần Nguyễn Thiên Phước <tntphuoc1608@gmail.com>
2026-03-06 23:10:48 +0700

    feat: add product-detail page

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,5 +5,6 @@ const productController = require('../controllers/productController');
 router.get('/', productController.index);
 router.get('/add', productController.showAddForm);
 router.post('/add', productController.addProduct);
+router.get('/:id', productController.detail);
 
 module.exports = router;
